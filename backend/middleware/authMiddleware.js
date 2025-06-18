89d9ff16343fddf488f85bd1290361a95d3b2533
shrashti-19 <shrashtidwivedi91@gmail.com>
2025-06-18 20:18:10 +0530

    updation of auth routes with jwt token

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,27 +1,47 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
 
-const protect = async (req, res, next) => {
+// const protect = async (req, res, next) => {
+//   let token;
+
+//   if (
+//     req.headers.authorization &&
+//     req.headers.authorization.startsWith('Bearer')
+//   ) {
+//     try {
+//       token = req.headers.authorization.split(' ')[1];
+
+//       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//       req.user = await User.findById(decoded.id).select('-password');
+//       next();
+//     } catch (error) {
+//       res.status(401).json({ error: 'Not authorized, invalid token' });
+//     }
+//   }
+
+//   if (!token) {
+//     res.status(401).json({ error: 'Not authorized, no token' });
+//   }
+// };
+
+// module.exports = {protect};
+
+export const protect = async (req,res,next)=>{
   let token;
 
-  if (
+  if(
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
-  ) {
-    try {
+  ){
+    try{
       token = req.headers.authorization.split(' ')[1];
-
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = await User.findById(decoded.id).select('-password');
-      next();
-    } catch (error) {
-      res.status(401).json({ error: 'Not authorized, invalid token' });
+      return next();
+    }catch(error){
+      return res.status(401).json({error: 'Not authorized, invalid token'});
     }
   }
 
-  if (!token) {
-    res.status(401).json({ error: 'Not authorized, no token' });
-  }
+  return res.status(401).json({error : 'Not authorized, no token'});
 };
-
-module.exports = {protect};
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,17 @@
 import express from 'express';
 import User from '../models/User.js';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
 
 const router = express.Router();
+
+//jwt token generator
+const generatorToken = (id)=>{
+  return jwt.sign({id}, process.env.JWT_SECRET,{
+    expiresIn: '7d',
+  });
+};
 
 // Register route
 router.post('/register', async (req, res) => {
@@ -34,7 +43,15 @@ router.post('/register', async (req, res) => {
 
     await newUser.save();
 
-    res.status(201).json({ message: 'User registered successfully' });
+    const token = generatorToken(newUser._id);
+    res.status(201).json({ message: 'User registered successfully' ,
+      user: {
+        id: newUser._id,
+        firstName: newUser.firstName,
+        email: newUser.email,
+      },
+      token,
+    });
   } catch (error) {
     console.error('Register Error:', error);
     res.status(500).json({ message: 'Server Error' });
@@ -63,7 +80,19 @@ router.post('/login', async (req, res) => {
     if(!isMatch){
         return res.status(400).json({message: "Invalid Password"});
     }
-    res.status(200).json({ message: 'Login successful', user: { firstName: user.firstName, email: user.email } });
+
+    //generating the token 
+    const token = generatorToken(user._id);
+    
+    res.status(200).json({
+      message: 'Login successful',
+      user:{
+        id: user._id,
+        firstName: user.firstName,
+        email: user.email,
+      },
+      token,
+    })
   } catch (error) {
     console.error('Login Error:', error);
     res.status(500).json({ message: 'Server Error' });
