b381201d819c779c843d9ab3853c25cb7b31891d
KushagraGaur <kushagragaur31@gmail.com>
2025-07-02 02:36:06 +0530

    minor fixed in dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState} from 'react';
 import { useTheme } from '../context/ThemeContext';
 import { useUser } from '../context/UserContext'; // Added to use centralized user data
 import Sidebar from '../components/Dashboard/Sidebar';
